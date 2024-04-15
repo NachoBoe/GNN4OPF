2f2374b627185fa655aabb09755e93be702fc6bb
Santiago Diaz <sdiazvaz@gmail.com>
2024-04-15 19:16:28 -0300

    add some code to psse.py for creating data of uruguay case

--- psse_scripts/psse.py
+++ psse_scripts/psse.py
@@ -9,30 +9,52 @@ ierr = psspy.case(sav_file_path)
 
 load_P_data = pandas.read_csv('./scripts/datos_ute_para_psse/PLOAD.csv')
 load_Q_data = pandas.read_csv('./scripts/datos_ute_para_psse/QLOAD.csv')
-
+gen_P_data = pandas.read_csv('./scripts/datos_ute_para_psse/PGEN.csv')
+gen_Q_data = pandas.read_csv('./scripts/datos_ute_para_psse/QGEN.csv')
 
 # Get list of columns
-columns_load = load_P_data.columns.tolist()
-for index, row in load_P_data.iterrows(): # iterar sobre el indice de cada fila y ahi ir y buscar en cada csv, cambiar como esta ahora
-    for column in columns_load:
-        Pload = row[column]
-        Qload = _
+columns_load_P = load_P_data.columns.tolist()
+columns_load_Q = load_Q_data.columns.tolist()
+comumns_gen_P = gen_P_data.columns.tolist()
+columns_gen_Q = gen_Q_data.columns.tolist()
+cant_datos = len(columns_load_P)
+
+# Values of default loads for buses that dont have Qload
+ierr, [load_numbers] = psspy.aloadint(sid=-1, flag=4, string="NUMBER")
+ierr, [loads] = psspy.aloadcplx(sid=-1, flag=4, string="TOTALACT") # si P es negativa va a 0 todo
+Qloads_dict = {}
+for load_number, load_value in zip(load_numbers, loads):
+    Qloads_dict[load_number] = load_value.imag
+
+# Values of default gens for buses that dont have Qgen
+# ierr, [machine_numbers] = psspy.amachint(sid=-1, flag=4, string="NUMBER")
+# ierr, [machineIDs] = psspy.amachchar(sid=-1, flag=4, string="ID")
+# ierr, [Pgen] = psspy.amachreal(sid=-1, flag=4, string="PGEN")
+
+for index in range(cant_datos):
+    for column in columns_load_P:
+        Pload = load_P_data[index][column]
+        if column in columns_load_Q:
+            Qload = load_Q_data[index][column]
+        else:
+            Qload = Qloads_dict[column]
+            Qload *= np.random.uniform(0.8, 1.2)
         ierr = psspy.load_data_3(int(column), "1", realar1=Pload, realar2=Qload)
+    for column in comumns_gen_P:
+        bus_number,id_gen = column.split('_')
+        Pgen = gen_P_data[index][bus_number]
+        if column in columns_gen_Q:
+            Qgen = gen_Q_data[index][bus_number]
+        else:
+            Qgen = 0 # si la Q gen no esta en datos UTE poner uniforme del valor default en psse
+        ierr = psspy.machine_data_2(int(bus_number), id_gen, realar1=Pgen, realar2=Qgen)
+
+    # Run power flow
+    ierr = psspy.fnsl()
+    ierr, [voltajes,angles] = psspy.abusreal(-1,2,string=["PU","ANGLED"]) 
+
+    # Run OPF
+
+    # Save data in  npy file
     break
 
-ierr, [machine_numbers] = psspy.amachint(sid=-1, flag=4, string="NUMBER")
-ierr, [machineIDs] = psspy.amachchar(sid=-1, flag=4, string="ID")
-ierr, [Pgen] = psspy.amachreal(sid=-1, flag=4, string="PGEN")
-
-for i, machine_number in enumerate(machine_numbers):
-    machine_id = machineIDs[i]
-    pgen = Pgen[i]
-    qgen = 0 # poner la qgen de los datos de la ute
-    ierr = psspy.machine_data_2(machine_number, machine_id, realar1=pgen, realar2=qgen)
-
-# Run power flow
-ierr = psspy.fnsl()
-ierr, [voltajes,angles] = psspy.abusreal(-1,2,string=["PU","ANGLED"])
-
-# Save the modified case
-# ierr = psspy.save("modified_case.sav")
