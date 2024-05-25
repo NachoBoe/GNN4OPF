38d7bf701365fa4e25b19aed9bf7d8f3b7d9ef4e
nacho <nacho.boero@gmail.com>
2024-05-25 17:55:52 -0300

    h

--- Entrenar/scripts/find_best_run.py
+++ Entrenar/scripts/find_best_run.py
@@ -31,30 +31,41 @@ def find_best_run_from_tfevents(path, k=10):
 
     return k_best_trains
 
-def find_best_run(path, k=10, metric = "val_ploss"):
+
+
+def find_best_run(path, k=10):
     '''
-    Encuentra los mejores k entrenamientos en un directorio, a partir de los info.json
+    Encuentra los mejores k entrenamientos en un directorio, a partir de los events.out.tfevents
     '''
-    best = []
+    val_losses = []
+    val_metrics = []
+    val_p_losses = []
     entrenamiento = []
 
-    for file in os.listdir(path):
-        with open('best_model_info.json', 'r') as f:
-            values = json.load(f)
+    for file_path in os.listdir(path):
 
         try:
-            key = values.get(metric)
-            best.append(key)
-            entrenamiento.append(file)
-
+            with open(os.path.join(path, file_path, 'best_model_info.json'), 'r') as file:
+                data = json.load(file)
+            
+            entrenamiento.append(file_path)
+            val_losses.append(data['val_loss'])
+            val_metrics.append(data['val_metric'])
+            val_p_loss.append(data['val_p_loss'])
         except:
             pass
 
-    indices_ordenados = sorted(range(len(best)), key=lambda i: best[i], reverse=False)
+    indices_ordenados = sorted(range(len(val_metrics)), key=lambda i: val_metrics[i], reverse=False)
     best_values = indices_ordenados[:k]
     k_best_trains = [entrenamiento[i] for i in best_values]
+    k_best_train_values = [val_metrics[i] for i in best_values]
+    
 
-    return k_best_trains
+
+    if 'best' not in k_best_trains[0]:
+        os.rename(os.path.join(path, k_best_trains[0]), os.path.join(path, 'best-'+k_best_trains[0]))
+
+    return zip(k_best_trains, k_best_train_values)
 
 
 if __name__ == "__main__":
@@ -65,5 +76,7 @@ if __name__ == "__main__":
 
     args = parser.parse_args()
 
-    k_best_trains = find_best_run_from_tfevents(args.folder_path, args.k)
-    print(k_best_trains)
+    best = find_best_run(args.folder_path, args.k)
+    #unzip best
+    for b in best:
+        print(b)
